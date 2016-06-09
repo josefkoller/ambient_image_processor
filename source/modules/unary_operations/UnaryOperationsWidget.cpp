d9f5db253ef83b8453707f4fd789c294d2361fa4
Josef Koller <josef.koller@sperlingweg.net>
2016-06-09 18:02:29 +0200

    added dct and idct to ui

--- source/modules/unary_operations/UnaryOperationsWidget.cpp
+++ source/modules/unary_operations/UnaryOperationsWidget.cpp
@@ -19,6 +19,11 @@ ITKImage UnaryOperationsWidget::processImage(ITKImage image)
 {
     if(this->ui->invert_checkbox->isChecked())
         return CudaImageOperationsProcessor::invert(image);
+
+    if(this->ui->dct_checkbox->isChecked())
+        return CudaImageOperationsProcessor::cosineTransform(image);
+    if(this->ui->idct_checkbox->isChecked())
+        return CudaImageOperationsProcessor::inverseCosineTransform(image);
 }
 
 void UnaryOperationsWidget::on_perform_button_clicked()
