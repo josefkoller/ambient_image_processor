8cedb582b9ca839209df995abb1e74bf79e18e57
Josef Koller <josef.koller@sperlingweg.net>
2017-01-08 09:37:38 +0100

    add source head to all cpp, h and cu files

--- source/modules/cuda_image_operations/CudaImageOperationsProcessor.h
+++ source/modules/cuda_image_operations/CudaImageOperationsProcessor.h
@@ -1,3 +1,24 @@
+/*
+    Ambient Image Processor - A tool to perform several imaging tasks
+    
+    Copyright (C) 2016 Josef Koller
+
+    https://github.com/josefkoller/ambient_image_processor    
+    
+    This program is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+*/
+
 #ifndef CUDAIMAGEOPERATIONSPROCESSOR_H
 #define CUDAIMAGEOPERATIONSPROCESSOR_H
 
